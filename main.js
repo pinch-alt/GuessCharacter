390df3a74c08917cfacf44f3b7546c384d0a24c1
ydk2007y <ydk2007y@gmail.com>
2026-05-07 19:05:17 +0000

    refactor: Increase mouse gravity strength by 5x

--- main.js
+++ main.js
@@ -384,7 +384,7 @@ class Game {
             const distToMouse = Vector2.distance(ent.pos, this.mousePos);
             if (distToMouse < 360) {
                 const forceDir = this.mousePos.copy().sub(ent.pos).normalize();
-                const forceMag = (360 - distToMouse) / 4437;
+                const forceMag = (360 - distToMouse) / 887.4;
                 const force = forceDir.multiply(this.isAttracting ? forceMag : -forceMag);
                 ent.applyForce(force);
             }
